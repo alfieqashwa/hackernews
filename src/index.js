5e09b842840c9cf08fce5ea2541f6240ddb83064
alfieqashwa <alfieqahwa@gmail.com>
2018-08-04 19:23:06 +0700

    Edited index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { EPROTO } from "constants";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
