580dea35e77f7976a0dd9691dc955ac3338e8702
alfieqashwa <alfieqashwa@gmail.com>
2018-08-04 19:28:22 +0700

    commented out "import EPROTO" in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { EPROTO } from "constants";
+// import { EPROTO } from "constants";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
